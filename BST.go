f52bed7ab93d44916c6e250a04c953eec5fa58b7
Max Base <maxbasecode@gmail.com>
2022-12-26 13:39:35 +0100

    update functions

--- BST.go
+++ BST.go
@@ -36,6 +36,7 @@ func insert(root *Node, value int) *Node {
  * @param value: The value of the node to be found.
  * @return: The node with the given value.
  */
+// O(logn) (worst case O(n))
 func find(root *Node, value int) *Node {
 	if root == nil {
 		return nil
@@ -50,4 +51,74 @@ func find(root *Node, value int) *Node {
 	} else {
 		return find(root.right, value)
 	}
+}
+
+/**
+ * @berif: Find next of the given node.
+ * @param root: The root of binary tree.
+ * @return: The next node of the given node.
+ */
+func findNext(root *Node, value int) *Node {
+	if root == nil {
+		return nil
+	}
+
+	node := find(root, value)
+	if node == nil {
+		return nil
+	}
+
+	// if the node has right child, find the leftmost node of the right child
+	if node.right != nil {
+		q := []*Node{node.right}
+		for len(q) > 0 {
+			n := q[0]
+			q = q[1:]
+			if n.left == nil {
+				return n
+			}
+			q = append(q, n.left)
+		}
+	}
+
+	// if the node has no right child, find the first node that is the left child of its parent
+	p := root
+	n := node
+	for p != nil {
+		if p.left == n {
+			return p
+		}
+		if n.value < p.value {
+			p = p.left
+		} else {
+			p = p.right
+		}
+	}
+	return nil
+
+/**
+ * @berif: Find next of the given node (for any Binary Tree)
+ * @param root: The root of binary tree.
+ * @param node: The given node.
+ * @return: The next node of the given node.
+ */
+func findNextAny(root *Node, node *Node) *Node {
+	if root == nil {
+		return nil
+	}
+
+	// for any binary tree, not only BST
+	if root.right == nil {
+		return nil
+	}
+	q := []*Node{root.right}
+	for len(q) > 0 {
+		n := q[0]
+		q = q[1:]
+		if n.left == nil {
+			return n
+		}
+		q = append(q, n.left)
+	}
+	return nil
 }
